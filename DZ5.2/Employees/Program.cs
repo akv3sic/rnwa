2a2e9513b60d9b288f0e858540c196b01dc05c77
Ante Kvesić <antekvesic2@gmail.com>
2022-05-25 19:53:57 +0200

    Kreiraj modele - reverse engineering

--- DZ5.2/Employees/Program.cs
+++ DZ5.2/Employees/Program.cs
@@ -1,7 +1,16 @@
+using Employees.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// dbContext
+string connString = builder.Configuration.GetConnectionString("employees");
+
+builder.Services.AddDbContext<employeesContext>(options =>
+  options.UseMySql(connString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.22-mariadb")));
 
 var app = builder.Build();
 
